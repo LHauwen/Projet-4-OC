48527df72206adfbaf939a6098a88961e28cfe4a
Louis Hauwen <hauwen.lou@gmail.com>
2021-08-31 10:42:07 +0200

    test css

--- templates/home.php
+++ templates/home.php
@@ -44,8 +44,16 @@ endif;
         </div>
     </nav>
     <header class="masthead">
-        <img src="../public/assets/img/Bg2.jpg">
-</header>
+            <div class="container position-relative px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-md-10 col-lg-8 col-xl-7">
+                        <div class="site-heading">
+                            <h1>Clean Blog</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
 <main class="container">
     <section class="row">
 
