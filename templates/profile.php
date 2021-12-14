896470c399f68219b7479c596d15977c24340357
Louis Hauwen <hauwen.lou@gmail.com>
2021-12-14 11:47:15 +0100

    style profile.php

--- templates/profile.php
+++ templates/profile.php
@@ -23,7 +23,7 @@ $user = $this->session->get('user');
 </svg>&ensp;Supprimer mon compte</a>
 </div>
 <br>
-<a class="btn btn-success" style="margin-top:50px;" href="../public/index.php"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-arrow-bar-left" viewBox="0 0 16 16">
+<a class="btn btn-secondary" style="margin-top:50px;" href="../public/index.php"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-arrow-bar-left" viewBox="0 0 16 16">
   <path fill-rule="evenodd" d="M12.5 15a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5zM10 8a.5.5 0 0 1-.5.5H3.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L3.707 7.5H9.5a.5.5 0 0 1 .5.5z"/>
 </svg>&ensp;Retour à l'accueil</a>
 </div>
